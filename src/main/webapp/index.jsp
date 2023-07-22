c58e714806bed050e835893cc9bbc667264f8c36
devOak28 <97941583+devOak28@users.noreply.github.com>
2023-07-22 10:24:03 +0530

    Initial commit

---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>MyApp</title>
+</head>
+<body>
+<center>
+    <h1>Hello, Jenkins App!</h1>
+</center>
+
+</body>
+</html>
---
+++ src/test/java/test/HomeControllerTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class HomeControllerTest {
+
+	  @Test  
+	    public void testName() {  
+	        assertEquals("Devesh", "Devesh");  
+	    } 
+	  
+	  @Test  
+	    public void testName2() {  
+	        assertEquals("Devesh", "Devesh");  
+	    } 
+	  
+	  @Test  
+	    public void testName3() {  
+	        assertEquals("Devesh", "devesh");  
+	    } 
+	
+}
